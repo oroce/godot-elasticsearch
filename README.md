c5be176a5d666a52cb9acac722a09b4cc51e115c
oroce <robert@oroszi.net>
2014-01-24 15:36:07 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 godot-elasticsearch
 ===================
+
+[ElasticSearch](http://www.elasticsearch.org/) producer for [godot](https://github.com/nodejitsu/godot/).
+
+It uses [@jesusabdullah](http://github.com/jesusabdullah)'s [godot-producer](https://github.com/jesusabdullah/godot-producer).
+
+How to use 
+-------------
+
+    var godot = require( "godot" );
+    var ElasticSearchProducer = require( "godot-elasticsearch" );
+    godot.createClient(
+        type: "udp",
+        producers: [
+            new ElasticSearchProducer({
+                host: "very.production.app.server.com",
+                service: "elasticsearch/health",
+                ttl: 10*15,
+
+                esHost: "localhost:9200" // ElasticSearch host
+
+            })
+        ]
+    ).connect( 1337, "localhost" );
+
+What type of events do `ElasticSearchProducer` emit?
+-------------------------
+
+- elasticsearch/health/healtcheck (if cluster isnt available this event contains critical state, otherwise it's ok)
+- elasticsearch/health/healtcheck/number_of_nodes
+- elasticsearch/health/healtcheck/number_of_data_nodes
+- elasticsearch/health/healtcheck/active_primary_shards
+- elasticsearch/health/healtcheck/active_shards
+- elasticsearch/health/healtcheck/relocating_shards
+- elasticsearch/health/healtcheck/initializing_shards
+- elasticsearch/health/healtcheck/data.unassigned_shards
+
+Are you lazy? Don't you wanna roll your on client?
+------------------------
+
+This module also install a binary called `godot-elasticsearch` to simplify the installation.
+
+run `npm install -g godot-elasticsearch && godot-elasticsearch -h localhost:9200 -g udp://localhost:1337` and watch the events \o/
+
+Debugging
+------------------------
+
+Run your godot client or godot-elasticsearch command with one of the following DEBUG envs:
+
+- DEBUG="godot:elasticsearch*": shows debug output both of the godot producer and the cli
+- DEBUG="godot:elasticsearch:producer": shows debug output of the godot producer
+- DEBUG="godot:elasticsearch:cli": show debug output of the cli
+
+LICENSE
+----------------------
+
+MIT
---
+++ index.js
@@ -0,0 +1,102 @@
+"use strict";
+
+var producer = require( "godot-producer" );
+var debug = require( "debug" )( "godot:elasticsearch:producer" );
+var elasticsearch = require( "elasticsearch" );
+
+var isUndefined = function( value ){
+  return typeof value === "undefined";
+}
+
+var ElasticSearchProducer = producer(
+  function constructor( options ){
+    options || ( options = {} );
+    debug( "creating client using options: %j", options );
+    this.serviceName = options.service;
+    this.client = new elasticsearch.Client({
+      host: options.esHost
+    });
+  },
+  function produce(){
+    var self = this;
+    var service = this.values.service;
+    debug( "produce" );
+
+    this.client.cluster.health(function( err, data ){
+      debug( "cluster response, err: %s, data: %j", err, data );
+      if( err ){
+        self.emit( "error", {
+          service: service + "/healthcheck" ,
+          state: "critical",
+          description: err.toString()
+        });
+        return;
+      }
+
+      var clusterName = data.cluster_name;
+      self.emit( "data", {
+        service: service + "/healthcheck",
+        description: clusterName,
+        meta: data
+      });
+
+      if( !isUndefined( data.number_of_nodes ) ){
+        self.emit( "data", {
+          service: service + "/number_of_nodes",
+          description: clusterName,
+          metric: data.number_of_nodes
+        });
+      }
+
+      if( !isUndefined( data.number_of_data_nodes ) ){
+        self.emit( "data", {
+          service: service + "/number_of_data_nodes",
+          description: clusterName,
+          metric: data.number_of_data_nodes
+        });
+      }
+
+      if( !isUndefined( data.active_primary_shards ) ){
+        self.emit( "data", {
+          service: service + "/active_primary_shards",
+          description: clusterName,
+          metric: data.active_primary_shards
+        });
+      }
+
+      if( !isUndefined( data.active_shards ) ){
+        self.emit( "data", {
+          service: service + "/active_shards",
+          description: clusterName,
+          metric: data.active_shards
+        });
+      }
+
+      if( !isUndefined( data.relocating_shards ) ){
+        self.emit( "data", {
+          service: service + "/relocating_shards",
+          description: clusterName,
+          metric: data.relocating_shards
+        });
+      }
+
+      if( !isUndefined( data.initializing_shards ) ){
+        self.emit( "data", {
+          service: service + "/initializing_shards",
+          description: clusterName,
+          metric: data.initializing_shards
+        });
+      }
+
+      if( !isUndefined( data.unassigned_shards ) ){
+        self.emit( "data", {
+          service: service + "/unassigned_shards",
+          description: clusterName,
+          metric: data.unassigned_shards
+        });
+      }
+    });
+  }
+);
+
+module.exports = ElasticSearchProducer;
